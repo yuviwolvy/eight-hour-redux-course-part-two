03044c13c2fe810a05f05d731058908ee1f12371
Yuvrajsinh Chauhan <yuvrajsinh.chauhan@rapidops.com>
2025-02-20 22:38:34 +0530

    Initial structure for starting react-redux

---
+++ src/components/pizzabox.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Pizzabox() {
+  return (
+    <div className="container">
+      <h2 className="text">Number of Pizza Available: 100</h2>
+      <button className="btn">Order Pizza</button>
+    </div>
+  );
+}
+
+export default Pizzabox;
